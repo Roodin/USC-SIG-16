fe536f321df2b72ffc0a83ce4a8075ac7b4c1711
Santi Argüeso <santi@comunitea.com>
2025-09-17 20:05:24 +0200

    [MIG] custom_multicompany

--- odoo/custom/src/private/custom_multicompany/__manifest__.py
+++ odoo/custom/src/private/custom_multicompany/__manifest__.py
@@ -7,7 +7,7 @@
     'summary': 'Custom SIG multicompany',
     'category': 'custom',
     'website': 'http://www.comunitea.com',
-    'version': '16.0.1.0.0',
+    'version': '18.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'product',
